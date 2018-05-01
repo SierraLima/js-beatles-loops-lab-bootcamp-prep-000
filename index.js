f922b8424d5f0a29cd8067eb32e142feffa3b28e
Stéphane <stephluethi@hotmail.com>
2018-05-01 07:57:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   
+  for ([initialization]; [condition]; [iteration]) {
+  [loopBody]
+}
+  
 }
 
 function johnLennonFacts(facts){
