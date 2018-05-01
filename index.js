3e2418d13d589aec939080544dd714842f07f426
Stéphane <stephluethi@hotmail.com>
2018-05-01 08:11:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   
   for ([initialization]; [condition]; [iteration]) {
-  array.push( musicians[1] + "plays" instruments[1] );
+  array.push(musicians[1] + "plays" + instruments[1] );
   
   console.log(entrepreneurs[2] + bio);
   }
